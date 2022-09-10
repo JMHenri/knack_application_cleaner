367ec6632545a2d2d5400a98ad9bf7e883580d94
jmhenri <jakehenri@gmail.com>
2022-09-09 20:52:34 -0400

    test file structure

---
+++ test/data-manipulation.js
@@ -0,0 +1,11 @@
+describe('duplicate detection', function() {
+  it('should detect version duplicates', function() {
+
+  });
+  it('should detect object duplicates', function() {
+    
+  });
+  it('should detect field duplicates', function() {
+
+  });
+});
